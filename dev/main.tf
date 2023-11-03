07d2e6a6246933deeba6bfe5fbfed71cf4107cb3
Sachin-Suresh-Hadadi <116323795+Sachin-Suresh-Hadadi@users.noreply.github.com>
2023-11-03 19:45:59 +0530

    Update main.tf

--- dev/main.tf
+++ dev/main.tf
@@ -115,19 +115,19 @@ resource "aws_security_group" "sg" {
         from_port   = 80
         to_port     = 80
         protocol    = "tcp"
-        cidr_blocks = ["180.151.249.18/32"]
+        cidr_blocks = [""]
   }
   ingress {
         from_port   = 22
         to_port     = 22
         protocol    = "tcp"
-        cidr_blocks = ["180.151.249.18/32"]
+        cidr_blocks = [""]
     }
   ingress {
         from_port   = 443
         to_port     = 443
         protocol    = "tcp"
-        cidr_blocks = ["180.151.249.18/32"]
+        cidr_blocks = [""]
     }
   egress {
         from_port   = 0
